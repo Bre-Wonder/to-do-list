ec2f37ca87d27398c7b997ab99e9147bc9f4ab02
Bre-Wonder <117392929+Bre-Wonder@users.noreply.github.com>
2023-01-15 15:57:34 -0800

    made changes to newItem function

--- To Do List/js/to-do-list-app.js
+++ To Do List/js/to-do-list-app.js
@@ -2,14 +2,21 @@ function newItem(){
 
   let li = $('<li></li>');
   let inputValue = $('input').value();
-  let text = 
+  let text = //jquery : $("#divButtons").append(?????); maybe this is my solution
   li.append(inputValue);
 
   if (inputValue === '') {
     alert('You must write something');
   } else {
     let list = $('#list');
-    list.append(li);
+    $('#list').append(li); // this line is wrong, but how come? becauseI defined list already
+  }
+
+  function crossOut() {
+    
   }
 
 }
+
+
+
