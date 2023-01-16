6ccce8d1505f6ee477103c21df0259b0912f59cb
Bre-Wonder <117392929+Bre-Wonder@users.noreply.github.com>
2023-01-15 16:34:02 -0800

    added delete and cross out functions

--- To Do List/js/to-do-list-app.js
+++ To Do List/js/to-do-list-app.js
@@ -2,19 +2,39 @@ function newItem(){
 
   let li = $('<li></li>');
   let inputValue = $('input').value();
-  let text = //jquery : $("#divButtons").append(?????); maybe this is my solution
+  //let text = jquery : $("#divButtons").append(?????); maybe this is my solution
   li.append(inputValue);
 
   if (inputValue === '') {
     alert('You must write something');
   } else {
     let list = $('#list');
-    $('#list').append(li); // this line is wrong, but how come? becauseI defined list already
+    $('#list').append(li); // how come you can't just use list.append(li) since you already?
   }
 
-  function crossOut() {
-    
+  li.on('dbclick', crossOut() => li.addClass('strike');)
+
+  let crossOutButton = $('<button></button>');
+  crossOutButton.addClass('crossOutButton');
+  crossOutButton.append(document.createTextNode('X'));
+  li.append(crossOutButton);
+
+  $('crossOutButton').on('click', deleteListItem);
+
+  function deleteListItem(){
+    li.addClass('delete');
   }
+
+  $('#list').sortable();
+
+
+
+  //function crossOut() {
+    //li.addClass('strike');
+ // }
+    
+
+  //li.addClass('delete');
 
 }
 
